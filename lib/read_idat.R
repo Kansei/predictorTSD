b39e81a028ec612c73f3d80c208431e290f55dbe
kansei <kk7221dev@gmail.com>
2019-01-15 18:56:35 +0900

    separate function

--- lib/read_idat.R
+++
@@ -1,7 +0,0 @@
-suppressPackageStartupMessages(require('methylumi'))
-
-idatPath <- "../data/E-MTAB-4664"
-
-barcodes <- scan("../data/E-MTAB-4664/barcodes.txt", what = character(), sep = "\n")
-
-idats <- methylumIDAT(barcodes = barcodes, idatPath=idatPath)
