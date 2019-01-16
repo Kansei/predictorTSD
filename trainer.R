734a7e9c1809312184223a3e065e12e49f1cdd3a
kansei <kk7221dev@gmail.com>
2019-01-16 22:54:44 +0900

    fix: model, normalize

--- trainer.R
+++ trainer.R
@@ -1,5 +1,5 @@
 source("./lib/utility.R")
-library(glmnet)
+source("./model.R")
 
 resource_name = 'E-MTAB-4664'
 
@@ -7,12 +7,12 @@ idats <- readIdats(resource_name)
 
 # To reformat matrix (barcode_name x cpg_sites)
 beta_value <- t(idats@assayData[["betas"]])
-m_value <- convertBeta2M(beta_value)
-X <- m_value
+m_value <- convertBeta2M(beta_value)i
+X <- normalize(m_value)
 
 attributes_data <- read.csv(paste("./data/", resource_name ,"/attributes.csv", sep = ""))
 
 Y <- convertSleepStatus2Num(attributes_data[5:6])
 
-lasso.model.cv <- cv.glmnet(x = X, y = Y, family = "binomial", alpha = 1)
+result <- model.lassoBinomial(X, Y)
 
