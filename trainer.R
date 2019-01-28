4aa36da130bbfa7874fc7db787331b6bdd51b096
kansei <kk7221dev@gmail.com>
2019-01-28 22:54:26 +0900

    fix

--- trainer.R
+++ trainer.R
@@ -2,9 +2,10 @@ source("./lib/utility.R")
 source("./model.R")
 
 resource_name = 'E-MTAB-4664'
-idats <- readIdats(resource_name)
+idats <- readIdatsObj(resource_name)
 
-# To reformat matrix (barcode_name x cpg_sites) <- normalize(idats)
+# To reformat matrix (barcode_name x cpg_sites)
+norm_idats <- normalize(idats)
 beta_value <- norm_idats@assayData[["betas"]]
 m_value <- convertBeta2M(beta_value)
 excepted_m_value <- exceptMissingValue(m_value)
@@ -13,16 +14,21 @@ X <- t(excepted_m_value)
 attributes_data <- read.csv(paste("./data/", resource_name ,"/attributes.csv", sep = ""))
 Y <- convertSleepStatus2Num(attributes_data[5:6])
 
-train_test <- trainTestSplit(X, Y, 0.3)
-train_X = train_test[1]
-test_X = train_test[2]
-train_Y = train_test[3]
-test_Y = train_test[4]
+# split test and train data
+test_size_rate <- 0.3
+data_size <- length(Y)
+test_size <- as.integer(data_size*test_size_rate)
+test_Y <- Y[1:test_size]
+train_Y <- Y[(test_size+1):data_size]
+test_X <- X[1:test_size,]
+train_X <- X[(test_size+1):data_size,]
 
-result <- model.lassoBinomial.cv(X, Y)
+result <- model.lassoBinomial.cv(train_X, train_Y)
 
 paramater <- coef(result, s="lambda.min")
+# paramater[which(paramater != 0), 1]
 
-paramater[which(paramater != 0),1]
+model_name <- paste(format(Sys.time(), "%Y%H%M%S"), ".rmodel", sep = "")
+model_path <- paste("./models/", model_name, sep = "")
+saveRDS(result, file = model_path)
 
-prd <- predict(result, newx = X, type = "class", s = "lambda.min")
