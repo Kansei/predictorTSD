ad4fec28ced5b973511c05fde05c63189bcb492e
kansei <kk7221dev@gmail.com>
2019-01-28 22:09:31 +0900

    install HDCI for bootLasso

--- installPackages.R
+++ installPackages.R
@@ -2,5 +2,5 @@ source("https://bioconductor.org/biocLite.R")
 biocLite("lumi")
 biocLite("methylumi")
 install.packages("glmnet", repos = "http://cran.us.r-project.org")
-
+install.packages("HDCI")
 
