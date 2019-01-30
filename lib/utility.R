0fc4c7d4f83244c67e024e6748913fbb9835e0bc
kansei <kk7221dev@gmail.com>
2019-01-30 16:33:20 +0900

    add: save and read model etc

--- lib/utility.R
+++ lib/utility.R
@@ -5,10 +5,23 @@ source("./lib/asmn/normalize_asmn.R")
 
 # Read idat files
 readIdatsObj = function(resource_name){
-  rdata_name <- paste(resource_name, ".rdata", sep = "")
-  rdata_path <- paste("./rdata/", rdata_name, sep = "")
+  rdata_path <- paste0("./rdata/", resource_name, ".rdata")
   idats <- readRDS(rdata_path)
   return(idats)
+}
+
+# Read saved model
+readModel = function(model_name){
+  model_path <- paste0("./models/", model_name, ".rmodel")
+  model <- readRDS(model_path)
+  return(model)
+}
+
+# Save model
+saveModel = function(model){
+  model_name <- format(Sys.time(), "%Y%m%d%H%M%S")
+  model_path <- paste0("./models/", model_name, ".rmodel")
+  saveRDS(result, file = model_path)
 }
 
 # Convert sleep status to 0 or 1
@@ -19,30 +32,30 @@ convertSleepStatus2Num = function(attributes_data){
   return(converted_data)
 }
 
-# convrt Beta-value to M-value
+# Convert Beta-value to M-value
 convertBeta2M = function(beta){
   m <- beta2m(beta)
   return(m)
 }
 
-# all sample normalize
+# All sample normalize
 normalize = function(idats){
   normfactors <- norm_factors(controldata = NULL, subjects = NULL, methylumidata = idats, type = "methylumi")
   normdata <- normalize_asmn(normfactors = normfactors, rawdata = NULL, featuredata = NULL, methylumidata = idats, type = "methylumi")
   return(normdata)
 }
 
-# except nan, inf
+# Except nan, inf
 exceptMissingValue = function(X){
   cpg_num <- length(X[,1])
-  nan <- which(is.nan(X)) %% 485577
-  inf <- which(is.infinite(X)) %% 485577
+  nan <- which(is.nan(X)) %% cpg_num
+  inf <- which(is.infinite(X)) %% cpg_num
   missing_value <- sort(unique(c(nan,inf)))
   excepted_X <- X[-missing_value, ]
   return(excepted_X)
 }
 
-# split test and train data
+# Split test and train data
 trainTestSplit = function(X, Y, test_size_rate){
   data_size <- length(Y)
   test_size <- as.integer(data_size*test_size_rate)
