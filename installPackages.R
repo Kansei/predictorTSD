f677cd4ae3cffc3e5d3dead8a62d88e8139a9426
kansei <kk7221dev@gmail.com>
2019-01-30 17:19:41 +0900

    install glm2

--- installPackages.R
+++ installPackages.R
@@ -1,6 +1,6 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite("lumi")
 biocLite("methylumi")
-install.packages("glmnet", repos = "http://cran.us.r-project.org")
+install.packages("glmnet")
+install.packages("glm2")
 install.packages("HDCI")
-
