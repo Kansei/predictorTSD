e6d7e7a4a8dd2b6a62b3e3bd2a3fdca0b9b93845
Kansei Kogami <kk7221dev@gmail.com>
2019-02-05 16:10:02 +0900

    fix: except some sample, adjust plot in plotMethylationBwSleepAndTSD

--- lib/plotMethylationBwSleepAndTSD.R
+++ lib/plotMethylationBwSleepAndTSD.R
@@ -23,6 +23,20 @@ tsd_samples <- tsd_samples[order(tsd_samples$individual),]
 sleep_samples <- attributes_data[-which_tsd_sample, ]
 sleep_samples <- sleep_samples[order(sleep_samples$individual),]
 
+except_tsd_samples <- c("9297962119_R04C01", "9297962119_R03C01", "9297962042_R04C01", "9297962042_R03C01")
+which_except_tsd_sample <- c()
+for(sample in except_tsd_samples){
+  which_except_tsd_sample <- append(which_except_tsd_sample, which(tsd_samples$Assay.Name == sample))
+}
+tsd_samples <- tsd_samples[-which_except_tsd_sample, ]
+
+except_sleep_samples <- c("9297962119_R02C02", "9297962119_R02C01", "9297962042_R02C02", "9297962042_R02C01")
+which_except_sleep_sample <- c()
+for(sample in except_sleep_samples){
+  which_except_sleep_sample <- append(which_except_sleep_sample, which(sleep_samples$Assay.Name == sample))
+}
+sleep_samples <- sleep_samples[-which_except_sleep_sample, ]
+
 ## cpgでfor文を回す
 for(ncpg in 1:nrow(tsd_specific_cpgs)){
   cpg_id <- as.character(tsd_specific_cpgs[ncpg, 1])
@@ -32,21 +46,29 @@ for(ncpg in 1:nrow(tsd_specific_cpgs)){
   min_m <- min(idats[, which_cpg])
 
   png(filename=paste0("./data/plot/methylation_bw_sleep_and_TSD/", cpg_id, ".png"))
-#
-  plot(c(0,1), c(min_m ,max_m),
+  par(mai=c(.8,.8,.8,1.2))
+  plot(c(0,1), c(min_m, max_m),
     type="n",
     ylim=c(min_m, max_m),
     xaxp=c(-1,1,2),
     xlab="TSD or Sleep", ylab="M-value",
-    main = cpg_id
+    main=cpg_id
   )
-  for(nsample in 1:nrow(tsd_samples)){
-   tsd_sample <- tsd_samples$Assay.Name[nsample]
-   sleep_sample <- sleep_samples$Assay.Name[nsample]
 
-   tsd_m <- idats[which(all_assay_names == tsd_sample), which_cpg]
-   sleep_m <- idats[which(all_assay_names == sleep_sample), which_cpg]
-   points(c(0,1), c(sleep_m, tsd_m), type="o")
+  labels <- c()
+  sample_range <- 1:nrow(tsd_samples)
+
+  for(nsample in sample_range){
+    tsd_sample_name <- tsd_samples$Assay.Name[nsample]
+    sleep_sample_name <- sleep_samples$Assay.Name[nsample]
+    labels <- append(labels, tsd_samples$individual[nsample])
+
+    tsd_m <- idats[which(all_assay_names == tsd_sample_name), which_cpg]
+    sleep_m <- idats[which(all_assay_names == sleep_sample_name), which_cpg]
+    points(c(0,1), c(sleep_m, tsd_m), type="o", pch=nsample, col=nsample)
   }
+
+  par(xpd=T)
+  legend(par()$usr[2], par()$usr[4], legend = labels, title = "Sample Id",col = sample_range, pch = sample_range, bg = "transparent")
   dev.off()
 }
