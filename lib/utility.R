919b3eb301555af704dc9e464bb3f3de17343108
Kansei Kogami <kk7221dev@gmail.com>
2019-02-05 17:19:58 +0900

    add: cpg selection function

--- lib/utility.R
+++ lib/utility.R
@@ -70,12 +70,12 @@ exceptMissingValue = function(X){
 trainTestSplit = function(X, Y, test_size_rate){
   data_size <- length(Y)
   test_size <- as.integer(data_size*test_size_rate)
-  
+
   test_Y <- Y[1:test_size]
   train_Y <- Y[(test_size+1):data_size]
   test_X <- X[1:test_size,]
   train_X <- X[(test_size+1):data_size,]
-  
+
   return(list(train_Y, test_Y, train_X, test_X))
 }
 
@@ -99,18 +99,40 @@ boLasso = function(x, y, B){
   if(length(selected_coef_position) == 0){
     stop("failed train\n")
   }
-  
+
   print("CpG site:")
   print(colnames(x[, selected_coef_position]))
   print("Coef:")
   print(result$Beta[selected_coef_position])
   print("Interval:")
   print(result$interval[1, selected_coef_position])
-  
+
   return(selected_coef_position)
 }
 
-rocAUC = function(predicted, y){ 
+tsdSpecificCpG = function(all_cpg_ids){
+  tsd_specific_cpg_ids <- scan("./data/DifferentiallyMethylatedProbes.txt", what = character(), sep = "\n")
+  which_tsd_cpgs <- c()
+  for(cpg_id in tsd_specific_cpg_ids){
+    which_tsd_cpgs <- append(which_tsd_cpgs, which(all_cpg_ids == cpg_id))
+  }
+  return(which_tsd_cpgs)
+}
+
+allCpG = function(){
+  return(TRUE)
+}
+
+cpgSelection.function = function(function_name){
+  switch(function_name,
+         "bolasso" = boLasso,
+         "tsdSpecificCpG" = tsdSpecificCpG,
+         "allCpG"= allCpG,
+         NULL
+         )
+}
+
+rocAUC = function(predicted, y){
   pred <- prediction(predicted, y)
   perf <- performance(pred, "tpr", "fpr")
   plot(perf)
--- trainer.R
+++ trainer.R
@@ -21,14 +21,20 @@ train_Y <- Y[(test_size+1):data_size]
 test_X <- X[1:test_size,]
 train_X <- X[(test_size+1):data_size,]
 
-# which_coef <- boLasso(train_X, train_Y, 10)
+function_name = "tsdSpecificCpG"
+cpgSelection <- cpgSelection.function(function_name)
+which_cpg <- cpgSelection(colnames(train_X))
+# boLasso(train_X, train_Y, 10)
+# tsdSpecificCpG(colnames(train_X))
+# allCpG()
 
 # Choose model
+# "logistic, "lassoBinomal.cv", "lassoBinomal"
 model_name = "logistic"
 model <- model.function(model_name)
-result <- model(train_X, train_Y)
+result <- model(train_X[,which_cpg], train_Y)
 
-pred.glm <- predict.glm(result, newdata = data.frame(test_X) ,type = "response")
+pred.glm <- predict.glm(result, newdata = data.frame(test_X[, which_cpg]) ,type = "response")
 auc <- rocAUC(pred.glm, test_Y)
 print("AUC")
 print(auc)
